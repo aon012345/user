6468ab11264b9633bfd85f3d2e945588e1b9ef07
aon012345 <62598276+aon012345@users.noreply.github.com>
2024-12-11 21:39:59 +0700

    Add files via upload

---
+++ endpoint/registration_login_db.sql
@@ -0,0 +1,34 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `tbl_user` (
+  `tbl_user_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+INSERT INTO `tbl_user` (`tbl_user_id`, `first_name`, `last_name`, `contact_number`, `email`, `username`, `password`) VALUES
+(2, 'Lorem', 'Ipsum', '2147483647', 'lorem.ipsum@gmail.com', 'admin', 'admin');
+INSERT INTO `tbl_user` (`tbl_user_id`, `first_name`, `last_name`, `contact_number`, `email`, `username`, `password`) VALUES
+(1, 'suasak', 'T', '0832235569', 'aon2.12345.jj@gmail.com', 'surasak', 'admin');
+
+
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`tbl_user_id`);
+
+
+ALTER TABLE `tbl_user`
+  MODIFY `tbl_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
---
+++ index.php
@@ -0,0 +1,125 @@
+<?php include ('./conn/conn.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Registration and Login System</title>
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="./assets/style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <div class="main">
+
+
+
+
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.6871796356268!2d100.51293489999999!3d13.7373794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29921f76193f7%3A0x5864886b1bdc3702!2z4LiWLiDguYDguKLguLLguKfguKPguLLguIog4LiB4Lij4Li44LiH4LmA4LiX4Lie4Lih4Lir4Liy4LiZ4LiE4Lij!5e0!3m2!1sth!2sth!4v1733838635690!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+
+
+
+        <!-- Login Area -->
+        <div class="login" id="loginForm">
+            <h1 class="text-center">Login Form</h1>
+            <div class="login-form">
+                <form action="./endpoint/login.php" method="POST">
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" id="username" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    <p class="registrationForm" onclick="showRegistrationForm()">No Account? Register Here.</p>
+                    <button type="submit" class="btn btn-dark login-btn form-control">Login</button>
+                </form>
+            </div>
+        </div>
+
+
+        <!-- Registration Area -->
+        <div class="registration" id="registrationForm">
+            <h1 class="text-center">Registration Form</h1>
+            <div class="registration-form">
+            <form action="./endpoint/add-user.php" method="POST">
+                <div class="form-group row">
+                    <div class="col-6">
+                        <label for="firstName">First Name:</label>
+                        <input type="text" class="form-control" id="firstName" name="first_name">
+                    </div>
+                    <div class="col-6">
+                        <label for="lastName">Last Name:</label>
+                        <input type="text" class="form-control" id="lastName" name="last_name">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-5">
+                        <label for="contactNumber">Contact Number:</label>
+                        <input type="number" class="form-control" id="contactNumber" name="contact_number" maxlength="11">
+                    </div>
+                    <div class="col-7">
+                        <label for="email">Email:</label>
+                        <input type="text" class="form-control" id="email" name="email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="registerUsername">Username:</label>
+                    <input type="text" class="form-control" id="registerUsername" name="username">
+                </div>
+                <div class="form-group">
+                    <label for="registerPassword">Password:</label>
+                    <input type="password" class="form-control" id="registerPassword" name="password">
+                </div>
+                <p class="registrationForm" onclick="showLoginForm()"><- Back</p>
+                <button type="submit" class="btn btn-dark login-register form-control">Register</button>
+            </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <script>
+        // Constant variables
+        const loginForm = document.getElementById('loginForm');
+        const registrationForm = document.getElementById('registrationForm');
+
+        // Hide registration form
+        registrationForm.style.display = "none";
+
+
+        function showRegistrationForm() {
+            registrationForm.style.display = "";
+            loginForm.style.display = "none";
+        }
+
+        function showLoginForm() {
+            registrationForm.style.display = "none";
+            loginForm.style.display = "";
+        }
+
+    </script>
+
+    <!-- Bootstrap Js -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+
+
+
+   
+
+</body>
+</html>
